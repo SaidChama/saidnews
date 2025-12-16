7356180bb9d56229462ce4a83ceffff8dc9b02c4
SaidChama <said.chama@gmail.com>
2025-12-16 19:32:50 -0300

    feat: finalized status endpoint

--- pages/api/v1/status/index.ts
+++ pages/api/v1/status/index.ts
@@ -1,11 +1,37 @@
 import database from "/infra/database.ts";
 
 async function status(request, response) {
-	const result = await database.query("SELECT 1 + 1 as SUM;");
-	console.log(result.rows);
-	response
-		.status(200)
-		.json({ message: "alunos do curso.dev são acima da média!" });
+	const updatedAt = new Date().toISOString();
+
+	const dbVersionResult = await database.query({
+		text: "SHOW server_version;",
+	});
+	const dbVersion = dbVersionResult.rows[0].server_version;
+
+	const maxConnectionsResult = await database.query({
+		text: "SHOW max_connections;",
+	});
+	const maxConnections = parseInt(
+		maxConnectionsResult.rows[0].max_connections,
+	);
+
+	const dbName = process.env.POSTGRES_DB;
+	const openedConnectionsResult = await database.query({
+		text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+		values: [dbName],
+	});
+	const openedConnections = openedConnectionsResult.rows[0].count;
+
+	response.status(200).json({
+		updated_at: updatedAt,
+		dependencies: {
+			database: {
+				version: dbVersion,
+				max_connections: maxConnections,
+				opened_connections: openedConnections,
+			},
+		},
+	});
 }
 
 export default status;
