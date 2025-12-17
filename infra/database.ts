c62ea41ba7f72e2b58c150f7641beaea27d1b96c
Said Chama <said.chama@gmail.com>
2025-12-17 11:26:09 -0300

    feat: added configuration for neon postres on prod

--- infra/database.ts
+++ infra/database.ts
@@ -7,13 +7,14 @@ async function query(queryObject) {
 		user: process.env.POSTGRES_USER,
 		database: process.env.POSTGRES_DB,
 		password: process.env.POSTGRES_PASSWORD,
+		ssl: process.env.POSTGRES_SSL === "development" ? false : true,
 	});
 	console.log("Credenciais do postgres:", {
 		host: process.env.POSTGRES_HOST,
 		port: process.env.POSTGRES_PORT,
 		user: process.env.POSTGRES_USER,
 		database: process.env.POSTGRES_DB,
-		password: process.env.POSTGRES_PASSWORD,
+		environment: process.env.NODE_ENV,
 	});
 	try {
 		await client.connect();
