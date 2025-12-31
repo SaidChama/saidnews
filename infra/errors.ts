1ff67c370f3e89e7b45b0f75c697ff01a0031f70
Said Chama <said.chama@gmail.com>
2025-12-31 17:19:24 -0300

    feat: add `MethodNotAllowedError` and `ServiceError`

--- infra/errors.ts
+++ infra/errors.ts
@@ -3,43 +3,71 @@ import { throws } from "node:assert";
 
 export class InternalServerError extends Error {
 	readonly action: string;
-	readonly status_code: number;
+	readonly statusCode: number;
 
-	constructor({ cause }: { cause?: unknown } = {}) {
+	constructor({
+		cause,
+		statusCode,
+	}: { cause?: unknown; statusCode?: any } = {}) {
 		super("Um erro interno não esperado ocorreu.", {
 			cause,
 		});
 		this.name = "InternalServerError";
 		this.action = "Entre em contato com o suporte";
-		this.status_code = 500;
+		this.statusCode = statusCode || 500;
 	}
 	toJSON() {
 		return {
 			message: this.message,
 			name: this.name,
 			action: this.action,
-			status_code: this.status_code,
+			status_code: this.statusCode,
+		};
+	}
+}
+
+export class ServiceError extends Error {
+	readonly action: string;
+	readonly statusCode: number;
+
+	constructor({
+		cause,
+		message,
+	}: { cause?: unknown; message?: string } = {}) {
+		super(message || "Serviço Indisponível no Momento", {
+			cause,
+		});
+		this.name = "ServiceError";
+		this.action = "Verifique se o Serviço está disponível.";
+		this.statusCode = 503;
+	}
+	toJSON() {
+		return {
+			message: this.message,
+			name: this.name,
+			action: this.action,
+			status_code: this.statusCode,
 		};
 	}
 }
 
 export class MethodNotAllowedError extends Error {
 	readonly action: string;
-	readonly status_code: number;
+	readonly statusCode: number;
 
 	constructor() {
 		super("Método não permitido para este endpoint.");
 		this.name = "MethodNotAllowedError";
 		this.action =
 			"Verifique se o método HTTP enviado é válido para este endpoint.";
-		this.status_code = 405;
+		this.statusCode = 405;
 	}
 	toJSON() {
 		return {
 			message: this.message,
 			name: this.name,
 			action: this.action,
-			status_code: this.status_code,
+			status_code: this.statusCode,
 		};
 	}
 }
