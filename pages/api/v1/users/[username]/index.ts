100436b3876df4bb4a1f5619ecb5567c02fe6396
SaidChama <said.chama@gmail.com>
2026-01-06 19:46:46 -0300

    feat: add support for `PATCH` on `/api/v1/users/[username]`

--- pages/api/v1/users/[username]/index.ts
+++ pages/api/v1/users/[username]/index.ts
@@ -6,6 +6,7 @@ import user from "models/user";
 const router = createRouter();
 
 router.get(getHandler);
+router.patch(patchHandler);
 
 export default router.handler(controller.errorHandlers);
 
@@ -15,4 +16,15 @@ async function getHandler(request: NextApiRequest, response: NextApiResponse) {
 	// const { password, ...publicUserRecord } = userRecord;
 	// return response.status(200).json(publicUserRecord);
 	return response.status(200).json(userRecord);
+}
+
+async function patchHandler(
+	request: NextApiRequest,
+	response: NextApiResponse,
+) {
+	const username = request.query.username;
+	const userInputValues = request.body;
+
+	const updatedUser = await user.update(username as string, userInputValues);
+	return response.status(200).json(updatedUser);
 }
