7e36ef710a3fc7f250fe2ad1ea2c04e3d9aaab19
SaidChama <said.chama@gmail.com>
2026-01-13 21:35:08 -0300

    feat: protect `/user/[username]` endpoint for `path` method

--- models/activation/index.ts
+++ models/activation/index.ts
@@ -118,6 +118,7 @@ async function activateUserByUserId(userId: string): Promise<UserRecord> {
 	const activatedUser = await user.setFeatures(userId, [
 		"create:session",
 		"read:session",
+		"update:user",
 	]);
 	return activatedUser;
 }
